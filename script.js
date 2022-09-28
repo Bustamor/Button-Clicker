48a962d87a106794c4c6d9fc4e36c7967101c5aa
Bustamor <randall.bustamante@gmail.com>
2022-09-28 13:51:47 -0700

    WebFunfirstcommit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Login to Logout
+
+function ChangeLoginToLogout(button_element){
+    button_element.innerText="logout"
+}
+// Remove definition element
+function removedef(elementname){
+    elementname.remove()
+}
